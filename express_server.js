a70241163b7e0faf343660f013fddae579e1585c
ChanTheo <theo.james.chan@gmail.com>
2020-01-20 21:50:30 +0000

    added another template that takes in a user event url and a simple post message in express_server.js

--- express_server.js
+++ express_server.js
@@ -2,7 +2,21 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+function generateRandomString() {
+  let result = "";
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  let charactersLength = characters.length
+
+  for (let index = 0; index < 6; index++) {
+    result += characters.charAt(Math.floor(Math.random()* charactersLength))
+    
+  }
+  return result;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -24,3 +38,26 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render('url_index', templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render('urls_new');
+});
+
+
+
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = {shortURL: req.param.shortURL, longURL: req.param.longURL};
+  res.render('url_show', templateVars);
+});
+
